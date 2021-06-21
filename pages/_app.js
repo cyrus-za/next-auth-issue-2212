631a55eaa545503b06c8b899222ef12af88c4181
Pieter Venter <pieter@venter.pro>
2021-06-21 12:52:29 +0200

    refactor: switch to SessionProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
-import { Provider } from 'next-auth/react'
+import { SessionProvider } from 'next-auth/react'
 import './styles.css'
 
-// Use the <Provider> to improve performance and allow components that call
+// Use the <SessionProvider> to improve performance and allow components that call
 // `useSession()` anywhere in your application to access the `session` object.
 export default function App ({ Component, pageProps }) {
   return (
-    <Provider
-      // Provider options are not required but can be useful in situations where
+    <SessionProvider
+      // SessionProvider options are not required but can be useful in situations where
       // you have a short session maxAge time. Shown here with default values.
       options={{
         // Client Max Age controls how often the useSession in the client should
@@ -25,6 +25,6 @@ export default function App ({ Component, pageProps }) {
       }}
       session={pageProps.session} >
       <Component {...pageProps} />
-    </Provider>
+    </SessionProvider>
   )
 }
